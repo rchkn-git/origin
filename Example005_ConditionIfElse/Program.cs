cf30cfb4aca34e2ab0a81fd165c1416e33b5db40
Aleksandr Ruchkan <aruchkan@yandex.ru>
2023-03-07 19:52:22 +0300

    Add Example005 Condition IfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string name = Console.ReadLine();
+if (name.ToLower() == "аня")
+{
+    Console.WriteLine("УРААОАОАОА!!!! ПРЕВЕД АНЯ!");
+}
+else
+{
+    Console.WriteLine("Привет, " + name + "!");
+}
