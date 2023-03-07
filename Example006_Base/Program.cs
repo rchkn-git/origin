cac9b13609409b2a69891b222ec01c86cab94ec5
Aleksandr Ruchkan <aruchkan@yandex.ru>
2023-03-07 19:57:35 +0300

    Add Example006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 4;
+int c = 2;
+int d = 7;
+int e = 3;
+int f = 0;
+
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+if (f > max ) max = f;
+
+Console.WriteLine("Max = " + max);
